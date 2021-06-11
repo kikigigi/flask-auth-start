e41e0f4f96ad70ec6bbe919a089fff12bc81b797
kikigigi <momokocute1@gmail.com>
2021-06-11 16:13:38 +0200

    "Add Procfile and modify requirements.txt"

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request, url_for, redirect, flash, sen
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import os
+
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'birdsandbees'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_AUTH_START_SECRET_KEY")
+print(f'key = {os.environ.get("FLASK_AUTH_START_SECRET_KEY")}')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
